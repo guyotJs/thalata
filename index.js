aad088d976da7522f2629809d7b42239758b473a
Weston <classicmclive@gmail.com>
2025-10-24 22:32:58 +0000

    Alpha 2

---
+++ index.js
@@ -0,0 +1,350 @@
+let footnotes = [
+    {
+    citation_number: 1,
+    scriptures: [
+      "Psalm 90:2",
+      "Isaiah 40:28",
+      "Revelation 22:13",
+      "Job 42:2",
+      "Jeremiah 32:17",
+      "Luke 1:37",
+      "Revelation 19:6",
+      "1 Samuel 2:3",
+      "Psalm 147:5",
+      "1 John 3:20",
+      "1 Kings 8:27",
+      "Proverbs 15:3",
+      "Numbers 23:19",
+      "Malachi 3:6",
+      "Psalm 102:25-27",
+      "Hebrew 1:10-12",
+      "Hebrews 13:8",
+      "John 1:1-3"
+    ]
+  },
+  {
+    citation_number: 2,
+    scriptures: [
+      "Isaiah 43:10",
+      "Exodus 20:3"
+    ]
+  },
+  {
+    citation_number: 3,
+    scriptures: [
+      "D&C 121:32",
+      "Abraham 3:23"
+    ]
+  },
+  {
+    citation_number: 4,
+    scriptures: ["§ II—As to the Succession of Godhood"]
+  },
+  {
+    citation_number: 5,
+    scriptures: [
+      "Alma 11:28",
+      "Alma 11:29",
+      "Deuteronomy 6:4",
+      "Isaiah 45:5",
+      "1 Timothy 2:5",
+      "Mark 12:29",
+      "James 2:19",
+      "Malachi 2:10",
+      "Romans 3:30"
+    ]
+  },
+  {
+    citation_number: 6,
+    scriptures: [
+      "Isaiah 45:5"
+    ]
+  },
+  {
+    citation_number: 7,
+    scriptures: ["See Index § III, C"]
+  },
+  {
+    citation_number: 8,
+    scriptures: [
+      "Matthew 16:18",
+      "John 16:3",
+      "Acts 2:42"
+    ]
+  },
+  {
+    citation_number: 9,
+    scriptures: [
+      "Numbers 23:19",
+      "Titus 1:2",
+      "Hebrews 6:18"
+    ]
+  },
+  {
+    citation_number: 10,
+    scriptures: [
+      "John 14:16-17"
+    ]
+  },
+  {
+    citation_number: 11,
+    scriptures: [
+      "Romans 8:14",
+      "John 16:13",
+      "Ephesians 1:13–14"
+    ]
+  },
+  {
+    citation_number: 12,
+    scriptures: ["See Index § V, A"]
+  },
+  {
+    citation_number: 13,
+    scriptures: ["§ IV—As to Apostasy in the Church"]
+  },
+  {
+    citation_number: 14,
+    scriptures: [
+      "Matthew 28:19-20",
+      "Hebrews 13:5",
+      "John 14:18",
+      "1 Thessalonians 4:16–17"
+    ]
+  },
+  {
+    citation_number: 15,
+    scriptures: [
+      "Jeremiah 17:9",
+      "Matthew 15:8-9",
+      "Proverbs 12:20",
+      "Mark 7:20-23",
+      "Psalm 52:8"
+    ]
+  },
+  {
+    citation_number: 16,
+    scriptures: [
+      "Moroni 10:3-5",
+      "3 Nephi 11:32",
+      "D&C 9:8-9"
+    ]
+  },
+  {
+    citation_number: 17,
+    scriptures: [
+      "2 Corinthians 11:14",
+      "2 Thessalonians 2:9-10"
+    ]
+  },
+  {
+    citation_number: 18,
+    scriptures: [
+      "Proverbs 23:12"
+    ]
+  },
+  {
+    citation_number: 19,
+    scriptures: [
+      "Proverbs 23:19"
+    ]
+  },
+  {
+    citation_number: 20,
+    scriptures: ["§ VII—On the Corruption of the Heart"]
+  },
+  {
+    citation_number: 21,
+    scriptures: ["§ III— As to Monarchical Monotheism"]
+  },
+  {
+    citation_number: 22,
+    scriptures: ["See Index § IX, C"]
+  },
+  {
+    citation_number: 23,
+    scriptures: [
+      "Alma 11:44",
+      "2 Nephi 31:21",
+      "Mosiah 15:1-4",
+      "Mormon 7:7"
+    ]
+  },
+  {
+    citation_number: 24,
+    "document": "The Testimony of Three Witnesses",
+    "source": "Book of Mormon"
+  },
+  {
+    citation_number: 25,
+    scriptures: [
+      "Alma 11:28",
+      "Alma 11:29"
+    ]
+  },
+  {
+    citation_number: 26,
+    scriptures: [
+      "Mosiah 15:1-4",
+      "Ether 3:14",
+      "Mosiah 16:15"
+    ]
+  },
+  {
+    citation_number: 27,
+    scriptures: [
+      "John 4:24"
+    ]
+  },
+  {
+    citation_number: 28,
+    scriptures: [
+      "Deuteronomy 5:25"
+    ]
+  },
+  {
+    citation_number: 29,
+    "source": "Summa Theologiae",
+    "reference": "I, q. 3, a. 2"
+  },
+  {
+    citation_number: 30,
+    scriptures: ["§ XII—As to the Incarnate Nature of the Father"]
+  },
+  {
+    citation_number: 31,
+    scriptures: [
+      "John 3:29",
+      "Revelation 19:7",
+      "Ephesians 5:25-27",
+      "Revelation 21:2"
+    ]
+  },
+  {
+    citation_number: 32,
+    scriptures: [
+      "Matthew 8:12",
+      "Matthew 25:41",
+      "2 Thessalonians 1:9",
+      "2 Peter 2:4",
+      "Revelations 14:11"
+    ]
+  },
+  {
+    citation_number: 33,
+    scriptures: [
+      "John 14:6",
+      "John 3:16",
+      "John 6:40",
+      "Acts 4:12",
+      "Romans 10:9",
+      "Matthew 7:21",
+      "1 John 5:11-12",
+      "John 10:9",
+      "John 8:24",
+      "Colossians 1:19-20"
+    ]
+  },
+  {
+    citation_number: 34,
+    scriptures: [
+      "2 Nephi 9:16-18",
+      "Alma 40:13-14",
+      "Mormon 9:3-5"
+    ]
+  },
+  {
+    citation_number: 35,
+    scriptures: [
+      "Matthew 19:23"
+    ]
+  },
+  {
+    citation_number: 36,
+    scriptures: [
+      "1 Peter 3:15",
+      "Jude 1:3"
+    ]
+  },
+  {
+    citation_number: 37,
+    scriptures: [
+      "Deuteronomy 6:6–7",
+      "Proverbs 22:6",
+      "Ephesians 6:4",
+      "Psalm 78:4–7"
+    ]
+  },
+  {
+    citation_number: 38,
+    scriptures: [
+      "Deuteronomy 18:20",
+      "Deuteronomy 18:22",
+      "Jeremiah 14:14",
+      "Jeremiah 23:16",
+      "Matthew 24:24"
+    ]
+  },
+  {
+    citation_number: 39,
+    scriptures: [
+      "James 3:1-12"
+    ]
+  },
+  {
+    citation_number: 40,
+    scriptures: [
+      "Acts 20:29"
+    ]
+  },
+  {
+    citation_number: 41,
+    scriptures: ["§ XVI—On the Failure of Prophecy"]
+  },
+  {
+    citation_number: 42,
+    scriptures: [
+      "D&C 84:5"
+    ]
+  },
+  {
+    citation_number: 43,
+    scriptures: [
+      "Luke 23:41-56"
+    ]
+  },
+  {
+    citation_number: 44,
+    scriptures: [
+      "1 Corinthians 1:18",
+      "Galatians 6:14",
+      "Romans 6:4–5",
+      "Philippians 3:10–11",
+      "Hebrews 12:2"
+    ]
+}]
+
+let fn = [
+  {
+    citation_number: 1,
+    scriptures: ["1 Corinthians 11:20-22"]
+  },
+  {
+    citation_number: 2,
+    scriptures: ["D&C 89:5"]
+  },
+  {
+    citation_number: 3,
+    scriptures: ["§ II—On the Ceremonial use of Wine by the Jews"]
+  },
+]
+
+let bibfn = [
+  {
+    citation_number: 1,
+    scriptures: ["Romans 11:4-5, Matthew 28:20"]
+  },
+  {
+    citation_number: 2,
+    scriptures: ["Acts 10; 13"]
+  },
+]
